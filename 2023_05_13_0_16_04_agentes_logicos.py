88fbb6a69176f9728265664ccc4ba1f792c9ef2a
Alnitakr51 <gadieljj34@gmail.com>
2023-05-14 02:28:05 +0200

    Lógica de 1er Orden

---
+++ 2023_05_13_0_16_04_agentes_logicos.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 14 01:13:45 2023
+
+@author: Gadiel Jimenez
+"""
+
+
+
+from pyDatalog import pyDatalog
+
+# Definir las reglas y hechos lógicos
+pyDatalog.create_terms('padre, madre, abuelo, hermano, hermana, X, Y, Z')
+
++padre('Juan', 'Pedro')
++madre('María', 'Pedro')
++padre('Pedro', 'Luis')
++madre('Ana', 'Luis')
+
+abuelo(X, Y) <= padre(X, Z) & padre(Z, Y)
+hermano(X, Y) <= padre(Z, X) & padre(Z, Y) & (X != Y)
+hermana(X, Y) <= madre(Z, X) & madre(Z, Y) & (X != Y)
+
+# Consultas
+print("Abuelos:")
+print(abuelo(X, Y))
+print()
+
+print("Hermanos:")
+print(hermano(X, Y))
+print()
+
+print("Hermanas:")
+print(hermana(X, Y))
